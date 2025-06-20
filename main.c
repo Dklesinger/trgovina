f1641163ba34316d4f430ec54c4801cea6f5c02f
Dklesinger <onlinerbx@gmail.com>
2025-06-20 02:05:03 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,59 +1,48 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include "header.h"
 
-// Deklaracije funkcija korištenih u main.c
-void meni_glavni(void);
+void meni_glavni(Komponenta** skladiste);
 void meni_admin(Komponenta** glava);
-void meni_korisnik(Komponenta* skladiste);  // Dodaj deklaraciju
 
 int main(void) {
-    Komponenta* skladiste = NULL;
+    Komponenta* skladiste = ucitaj_komponente_iz_datoteke("komponente.bin");
 
-    skladiste = ucitaj_komponente_iz_datoteke("komponente.bin");
+    meni_glavni(&skladiste);
 
-    meni_glavni();
+    if (!sacuvaj_komponente_u_datoteci("komponente.bin", skladiste)) {
+        printf("Greška pri spremanju komponenti.\n");
+    }
 
     oslobodi_komponente(skladiste);
     return 0;
 }
 
-void meni_glavni(void) {
-    Komponenta* skladiste = NULL;
+void meni_glavni(Komponenta** skladiste) {
     int izbor;
     bool kraj = false;
 
-    skladiste = ucitaj_komponente_iz_datoteke("komponente.bin");
-
     while (!kraj) {
         printf("\n--- Glavni izbornik ---\n");
-        printf("1. Administracija\n");
-        printf("2. Korisnicki izbornik \n");
-        printf("0. Izlaz\n");
+        printf("%d. Administracija\n", IZBORNIK_ADMIN);
+        printf("%d. Korisnicki izbornik \n", IZBORNIK_KORISNIK);
+        printf("%d. Izlaz\n", IZBORNIK_IZLAZ);
         izbor = ucitaj_int("Odaberite opciju: ");
 
         switch (izbor) {
-        case 1:
-            meni_admin(&skladiste);
+        case IZBORNIK_ADMIN:
+            meni_admin(skladiste);
             break;
-        case 2:
-            meni_korisnik(skladiste);
+        case IZBORNIK_KORISNIK:
+            meni_korisnik(*skladiste);
             break;
-        case 0:
+        case IZBORNIK_IZLAZ:
             kraj = true;
             break;
         default:
             printf("Nepoznata opcija.\n");
         }
     }
-
-    if (!sacuvaj_komponente_u_datoteku("komponente.bin", skladiste)) {
-        printf("Greška pri spremanju komponenti.\n");
-    }
-
-    oslobodi_komponente(skladiste);
 }
-
-void meni_korisnik(Komponenta* skladiste);
